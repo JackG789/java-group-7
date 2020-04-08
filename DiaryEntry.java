83246386f65fb6390a4e61f9d3733fe81530fbe7
JackG789 <jxglen@dundee.ac.uk>
2020-04-08 19:10:17 +0100

    object for each diary

--- DiaryEntry.java
+++ DiaryEntry.java
@@ -6,6 +6,8 @@ public class DiaryEntry extends HealthProfessional {
 
 	public Diary(int newid, String newname, String newworkArea, String newprofession) {
 		super(newid, newname, newworkArea, newprofession);
+		DiaryEntry diaryOne = new DiaryEntry(newid, newprofession, newprofession, newprofession);
+
 		// TODO Auto-generated constructor stub
 	}
 
